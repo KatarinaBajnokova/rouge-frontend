78427702c646362bd34d70de734b3f525bff5653
Wawa755 <146659430+Wawa755@users.noreply.github.com>
2025-05-22 01:12:57 +0200

    subcategories styling

--- src/react/components/all/SubcategoryItem.jsx
+++ src/react/components/all/SubcategoryItem.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
 import { Divider } from '@mantine/core';
 import { IconChevronRight } from '@tabler/icons-react';
-
 import '@/sass/components/all/_subcategory_item.scss';
 
 export default function SubcategoryItem({ label, onClick, color = '#317AFF' }) {
   return (
-    <div className='subcategory-item' onClick={onClick}>
-      <div className='item-content'>
-        <span className='circle-indicator' style={{ backgroundColor: color }} />
+    <div className="subcategory-item" onClick={onClick}>
+      <div className="item-content">
+        <span className="circle-indicator" style={{ backgroundColor: color }} />
         <p>{label}</p>
-        <IconChevronRight size={24} stroke={2} />
+        <IconChevronRight size={30} stroke={2} />
       </div>
-      <Divider />
+      <Divider className="category-divider" />
     </div>
   );
 }
